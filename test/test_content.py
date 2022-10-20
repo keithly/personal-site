65fb73037f51c91683220e0166acca06b0e74431
Keith Petersen <krp@keithrpetersen.com>
2022-10-20 10:16:07 -0500

    rewrite test_content use pathlib, pytest, and tmp_path

--- test/test_content.py
+++ test/test_content.py
@@ -1,62 +1,64 @@
-import unittest
-import shutil
-import os
-
+from pathlib import Path
 import makesite
-from test import path
+
+import pytest
 
 
-class ContentTest(unittest.TestCase):
-    def setUp(self):
-        self.blog_path = path.temppath("blog")
-        self.undated_path = os.path.join(self.blog_path, "foo.txt")
-        self.dated_path = os.path.join(self.blog_path, "2018-01-01-foo.txt")
-        self.header_post_path = os.path.join(self.blog_path, "baz.txt")
-        self.md_post_path = os.path.join(self.blog_path, "qux.md")
+@pytest.fixture
+def input(tmp_path: Path):
+    i_path = tmp_path / "blog"
+    i_path.mkdir()
+    return i_path
 
-        os.makedirs(self.blog_path)
 
-        with open(self.undated_path, "w") as f:
-            f.write("hello world")
+@pytest.fixture
+def undated(input: Path):
+    _undated = input / "foo.md"
+    _undated.write_text("*hello* world")
+    return _undated
 
-        with open(self.dated_path, "w") as f:
-            f.write("hello world")
 
-        with open(self.header_post_path, "w") as f:
-            f.write("<!-- a: 1 -->\n<!-- b: 2 -->\nFoo")
+@pytest.fixture
+def dated(input: Path):
+    _dated = input / "2018-01-01-foo.md"
+    _dated.write_text("hello world")
+    return _dated
 
-        with open(self.md_post_path, "w") as f:
-            f.write("*Foo*")
 
-    def tearDown(self):
-        shutil.rmtree(self.blog_path)
+@pytest.fixture
+def header_post(input: Path):
+    _header = input / "baz.md"
+    _header.write_text("<!-- a: 1 -->\n<!-- b: 2 -->\nFoo")
+    return _header
 
-    def test_content_content(self):
-        content = makesite.read_content(self.undated_path)
-        self.assertEqual(content["content"], "<p>hello world</p>\n")
 
-    def test_content_date(self):
-        content = makesite.read_content(self.dated_path)
-        self.assertEqual(content["date"], "2018-01-01")
+def test_content_content(undated):
+    content = makesite.read_content(undated)
+    assert content["content"] == "<p><em>hello</em> world</p>\n"
 
-    def test_content_date_missing(self):
-        content = makesite.read_content(self.undated_path)
-        self.assertEqual(content["date"], "1970-01-01")
 
-    def test_content_slug_dated(self):
-        content = makesite.read_content(self.dated_path)
-        self.assertEqual(content["slug"], "foo")
+def test_content_date(dated):
+    content = makesite.read_content(dated)
+    assert content["date"] == "2018-01-01"
 
-    def test_content_slug_undated(self):
-        content = makesite.read_content(self.undated_path)
-        self.assertEqual(content["slug"], "foo")
 
-    def test_content_headers(self):
-        content = makesite.read_content(self.header_post_path)
-        self.assertEqual(content["a"], "1")
-        self.assertEqual(content["b"], "2")
-        self.assertEqual(content["content"], "<p>Foo</p>\n")
+def test_content_date_missing(undated):
+    content = makesite.read_content(undated)
+    assert content["date"] == "1970-01-01"
 
-    def test_markdown_rendering(self):
-        content = makesite.read_content(self.md_post_path)
-        self.assertEqual(content["content"], "<p><em>Foo</em></p>\n")
+
+def test_content_slug_dated(dated):
+    content = makesite.read_content(dated)
+    assert content["slug"] == "foo"
+
+
+def test_content_slug_undated(undated):
+    content = makesite.read_content(undated)
+    assert content["slug"] == "foo"
+
+
+def test_content_headers(header_post):
+    content = makesite.read_content(header_post)
+    assert content["a"] == "1"
+    assert content["b"] == "2"
+    assert content["content"] == "<p>Foo</p>\n"
