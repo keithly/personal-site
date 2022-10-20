73f2ac3263e4b95d4fc9870ee32d4d938a1c36f3
Keith Petersen <krp@keithrpetersen.com>
2022-10-20 10:44:32 -0500

    rewrite more tests use pathlib, pytest, and tmp_path

--- test/test_file_io.py
+++ test/test_file_io.py
@@ -1,39 +1,36 @@
-import unittest
-import os
-import shutil
+from pathlib import Path
+
 
 import makesite
-from test import path
 
 
-class FileIOTest(unittest.TestCase):
-    """Tests for file I/O functions."""
+def test_fread(tmp_path: Path):
+    text = "foo\nbar\n"
+    filepath = tmp_path / "foo.txt"
+    filepath.write_text(text)
 
-    def test_fread(self):
-        text = "foo\nbar\n"
-        filepath = path.temppath("foo.txt")
-        with open(filepath, "w") as f:
-            f.write(text)
-        text_read = makesite.fread(filepath)
-        os.remove(filepath)
-        self.assertEqual(text_read, text)
+    text_read = makesite.fread(filepath)
 
-    def test_fwrite(self):
-        text = "baz\nqux\n"
-        filepath = path.temppath("foo.txt")
-        makesite.fwrite(filepath, text)
-        with open(filepath) as f:
-            text_read = f.read()
-        os.remove(filepath)
-        self.assertEqual(text_read, text)
+    assert text_read == text
 
-    def test_fwrite_makedir(self):
-        text = "baz\nqux\n"
-        dirpath = path.temppath("foo", "bar")
-        filepath = os.path.join(dirpath, "foo.txt")
-        makesite.fwrite(filepath, text)
-        with open(filepath) as f:
-            text_read = f.read()
-        self.assertTrue(os.path.isdir(dirpath))
-        shutil.rmtree(path.temppath("foo"))
-        self.assertEqual(text_read, text)
+
+def test_fwrite(tmp_path: Path):
+    text = "baz\nqux\n"
+    filepath = tmp_path / "foo.txt"
+
+    makesite.fwrite(filepath, text)
+
+    text_read = filepath.read_text()
+    assert text_read == text
+
+
+def test_fwrite_makedir(tmp_path: Path):
+    text = "baz\nqux\n"
+    dirpath = tmp_path / "foo" / "bar"
+    filepath = dirpath / "foo.txt"
+
+    makesite.fwrite(filepath, text)
+
+    text_read = filepath.read_text()
+    assert dirpath.is_dir() is True
+    assert text_read == text
